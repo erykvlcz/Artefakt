c6535efa07bda5e879ad679a2f6f44f7651f1146
Eryk Kowalczyk <kowalczykeryk@op.pl>
2025-05-07 23:11:54 +0200

    developed classes

--- src/main/java/pl/kurs/Artefakt.java
+++ src/main/java/pl/kurs/Artefakt.java
@@ -1,6 +1,30 @@
 package pl.kurs;
 
+import java.util.Set;
+
 public class Artefakt {
+    private String name;
+    private int powerLevel;
+    private ArtefaktType artefaktType;
+    private Set<ArtefaktProperty> artefaktPropertySet;
 
+    public Artefakt(String name, int powerLevel, ArtefaktType artefaktType) {
+        this.name = name;
+        this.powerLevel = powerLevel;
+        this.artefaktType = artefaktType;
+    }
 
+    public void addToArtefaktProperty(ArtefaktProperty property){
+        artefaktPropertySet.add(property);
+    }
+
+    @Override
+    public String toString() {
+        return "Artefakt{" +
+                "name='" + name + '\'' +
+                ", powerLevel=" + powerLevel +
+                ", artefaktType=" + artefaktType +
+                ", artefaktPropertySet=" + artefaktPropertySet +
+                '}';
+    }
 }
